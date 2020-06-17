9928f93ccd77037aafd498fca920c8e177b85090
Eduardo <eddeav98@gmail.com>
2020-06-16 20:17:47 -0500

    DriverLCD

---
+++ MKL25Z128xxx4_LCD_Driver/source/MKL25Z128xxx4_LCD_Driver.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include "board.h"
+#include "peripherals.h"
+#include "pin_mux.h"
+#include "clock_config.h"
+#include "MKL25Z4.h"
+#include "fsl_debug_console.h"
+#include "LCD.h"
+
+
+int main(void) {
+
+
+    BOARD_InitBootPins();
+    BOARD_InitBootClocks();
+    BOARD_InitBootPeripherals();
+    BOARD_InitDebugConsole();
+
+    LCD_Activate(1u,1u,0u);
+    LCD_Set(1u,1u,1u);
+        LCD_Clear();
+        Line(1);
+        LCD_Write('M');
+        LCD_Write('I');
+        LCD_Write('C');
+        LCD_Write('R');
+        LCD_Write('O');
+        LCD_Write('S');
+        LCD_Write(' ');
+        LCD_Write(' ');
+        LCD_Write('I');
+        LCD_Write('E');
+        LCD_Write('I');
+        LCD_Write(' ');
+        LCD_Write(' ');
+        LCD_Write('U');
+        LCD_Write('A');
+        LCD_Write('Z');
+        Line(2);
+        LCD_Write('_');
+        LCD_Write('<');
+        LCD_Write('A');
+        LCD_Write('S');
+        LCD_Write('T');
+        LCD_Write('R');
+        LCD_Write('O');
+        LCD_Write('C');
+        LCD_Write('O');
+        LCD_Write('D');
+        LCD_Write('E');
+        LCD_Write('>');
+
+
+    volatile static int i = 0 ;
+
+    while(1) {
+        i++ ;
+        __asm volatile ("nop");
+    }
+    return 0 ;
+}
